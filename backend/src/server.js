d38eb9b6036e724e132aeafffb4e36b4ac283dc4
CodeYourLifeWith-BHAWII <pbhawna2023@gmail.com>
2026-07-23 06:05:48 +0530

    initialize Express server with Clerk authentication and multi-stage Docker configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,17 @@ import "dotenv/config"
 import { connectDB } from "./lib/db.js";
 import {clerkMiddleware} from "@clerk/express"
 import cors from "cors"
+import fs from "fs"
+import path from "path"
+
 const app = express();
 const port = process.env.PORT||"4000";
 const FRONTEND_URL = process.env.FRONTEND_URL
+
+const publicDir = path.join(process.cwd(),"public")
+
+
+
 app.use(express.json())//this line is used to parse the incoming requests with JSON payloads , it means express.json will convert the json into normal js object.
 
 app.use(cors({
@@ -20,6 +28,18 @@ app.get("/health",(req,res)=>{
         ok:true
     })
 })
+
+//if the public directory exists, serve the static files 
+//this is for the production build
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir))
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(
+            path.join(publicDir,"index.html"),
+            (err)=>next(err)
+        )
+    })
+}
 
 app.listen(port,()=>{
     connectDB()
